7773a97bd8c89c8a394988ae5b4a50041a96487a
charlag <ivan.kupalov@icloud.com>
2018-02-11 00:11:06 +0300

    Add rating calculation. Cleanup

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const fs = require('fs')
 const crc32 = require('crc-32')
 
-const NUM_HASHES = 10
+// This number should probably be calculated from the set size
+const NUM_HASHES = 20
+const MAX_SHINGLE_ID = Math.pow(2, 32) - 1
+// This one should be found by some heuristic too
+const SUSPICIOUS_COLLISION_THRESHOLD = 0.45
 
-const arrayRange = (max) => Array.from(new Array(max), (x, i) => i)
-
+/**
+ * Calculates number of the equal elements in the same positions. Similar to
+ * set intersection but takes position into account.
+ * @param {array} arr1 The first array
+ * @param {array} arr2 The second array
+ * @returns {number} Number of the positions with equal elements
+ */
 const arrayIntersectionSize = (arr1, arr2) => {
-  const arrSize = arr1.length
+  const arrSize = Math.min(arr1.length, arr2.length)
   let count = 0
   for (let i = 0; i < arrSize; i++) {
     if (arr1[i] === arr2[i]) {
@@ -15,6 +24,13 @@ const arrayIntersectionSize = (arr1, arr2) => {
   }
   return count
 }
+
+/**
+ * Calculate average of the numeric array (sum / length)
+ * @param {array<number>} arr Array to calculate avarage of
+ * @return {number} Average of the array
+ */
+const average = (arr) => arr.reduce((acc, el) => acc + el, 0) / arr.length
 
 /**
  * Make ngrams [sequences of length n]
@@ -37,54 +53,62 @@ const makeNgrams = (values, length) => {
  * @returns {array<array<string>>} Array of ngrams (arrays of words)
  */
 const makeWordNgrams = (document) => {
-  const words = document.split(' ')
+  const words = document.toLowerCase().split(' ')
   return makeNgrams(words, 3)
 }
 
-const shuffleArray = (array) => {
-  for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1))
-    let temp = array[i]
-    array[i] = array[j]
-    array[j] = temp
-    }
-}
+/**
+ * Get random integer in the range [0, max)
+ * @param {number} max Upper bound of the number range
+ * @return {number} Random number in the specified range
+ */
+const getRandomInt = (max) =>
+      Math.floor(Math.random() * Math.floor(max))
 
-const generateRandomCoeffs = (size) => {
-  const array = arrayRange(size)
-  shuffleArray(array)
+/**
+ * Generate an array of random numbers in the range [0, maxId). They are
+ * checked to be uniquie in the array.
+ * @param {number} Size Size of the array which will be generated
+ * @param {number} Upper bound of the possible numbers
+ * @return {array} Array of unique random numbers in specified range
+ */
+const generateRandomCoeffs = (size, maxId) => {
+  const array = new Array(size)
+  for (let i = 0; i < size; i++) {
+    let randIndex
+    do {
+      randIndex = getRandomInt(maxId)
+    } while (array.indexOf(randIndex) !== -1)
+    array[i] = randIndex
+  }
   return array
 }
 
-const documents = fs.readFileSync('data/articles_100.train', 'utf8')
+const documents = fs.readFileSync('data/mails.train', 'utf8')
       .split('\n')
-      .reduce((accum, line) => {
+      .map(line => {
         const idLength = line.indexOf(' ')
         const id = line.substring(0, idLength)
         const content = line.substring(idLength + 1)
-        accum[id] = content
-        return accum
-      }, {})
+        return {
+          id,
+          content
+        }
+      })
 
-const shingles = Object.keys(documents).reduce((acc, key) => {
-  const content = documents[key] // string with content
+const shingles = documents.map(({content}) => {
   const ngrams = makeWordNgrams(content)
-  const shingles = ngrams.map(ngram => {
+  const shinglesForDoc = ngrams.map(ngram => {
     const [f, s, t] = ngram
     const ngramStr = `${f} ${s} ${t}`
     return crc32.str(ngramStr)
   })
-  acc[key] = shingles
-  return acc
-}, {})
-
-// const totalShingles = Object.keys(shingles).reduce((acc, key) => {
-//   return acc + shingles[key].length
-// }, 0)
+  return shinglesForDoc
+})
 
 const documentsSize = Object.keys(documents).length
-const coeffA = generateRandomCoeffs(documentsSize)
-const coeffB = generateRandomCoeffs(documentsSize)
+const coeffA = generateRandomCoeffs(documentsSize, MAX_SHINGLE_ID)
+const coeffB = generateRandomCoeffs(documentsSize, MAX_SHINGLE_ID)
 
 const signatures = (() => {
   let nextPrime = 4294967311
@@ -92,9 +116,7 @@ const signatures = (() => {
   const universalHash = (i, shingle) =>
         (coeffA[i] * shingle + coeffB[i]) % nextPrime
 
-  return Object.keys(shingles).map(key => {
-    const shinglesForDoc = shingles[key]
-
+  return shingles.map(shinglesForDoc => {
     const signature = []
 
     for (let i = 0; i < NUM_HASHES; i++) {
@@ -115,19 +137,36 @@ const signatures = (() => {
 })()
 
 // documentsSize * documentsSize matrix
-const estimates = Array.from(new Array(documentsSize), (i) => {
+const estimates = Array.from(Array(documentsSize), (i) => {
   return new Array(documentsSize)
 })
 
-for (let i = 0; i < documentsSize; i++) {
+const emailsRating = {}
+
+for (let i = 0; i < documentsSize - 1; i++) {
   const firstSignature = signatures[i]
   for (let j = i + 1; j < documentsSize; j++) {
     const secondSignature = signatures[j]
     const intersectionSize = arrayIntersectionSize(firstSignature, secondSignature)
     const estimate = intersectionSize / NUM_HASHES
     estimates[i][j] = estimate
-    if (estimate > 0.5) {
-      console.log(`Estimate ${i} ${j} is high`)
-    }
+    const dangerous = estimate >= SUSPICIOUS_COLLISION_THRESHOLD
+    const warnSign = dangerous ? '!' : ''
+    console.log(`${documents[i].id} & ${documents[j].id}: ${Number(estimate).toFixed(2)}      ${warnSign}`)
+    const prevI = emailsRating[i] || 0
+    const prevJ = emailsRating[j] || 0
+    const addScoreI = dangerous ? prevI + estimate + 0.1 : 0
+    const addScoreJ = dangerous ? prevJ + estimate + 0.1 : 0
+    emailsRating[i] = prevI + addScoreI
+    emailsRating[j] = prevJ + addScoreJ
   }
+  console.log('------------')
+}
+
+const averageRating = average(Object.values(emailsRating))
+console.log(`Average rating: ${averageRating}`)
+console.log('Emails rating: ')
+for (let i = 0; i < documentsSize; i++) {
+  const rating = emailsRating[i]
+  console.log(`${documents[i].id}: ${rating}`)
 }
